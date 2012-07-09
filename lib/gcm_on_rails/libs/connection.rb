3c43b8f414cd2ad5a7a4621e51103ee818b68e74
Dennis Ondeng <dennis@kopokopo.com>
2012-07-09 18:53:54 +0300

    Documentation for Gcm on Rails

--- lib/gcm_on_rails/libs/connection.rb
+++ lib/gcm_on_rails/libs/connection.rb
@@ -12,8 +12,17 @@ module Gcm
 
           data = notification.data.merge({"collapse_key" => notification.collapse_key}) unless notification.collapse_key.nil?
           data = data.merge({"delay_while_idle" => notification.delay_while_idle}) unless notification.delay_while_idle.nil?
+          data = data.merge({"time_to_live" => notification.time_to_live}) unless notification.time_to_live.nil?
           data = data.to_json
         else   #plain text format
+          headers = {"Content-Type" => "application/x-www-form-urlencoded;charset=UTF-8",
+                     "Authorization" => "key=#{api_key}"}
+
+          post_data = notification.data[:data].map{|k, v| "&data.#{k}=#{URI.escape(v)}".reduce{|k, v| k + v}}[0]
+          extra_data = "registration_id=#{notification.data[:registration_ids][0]}"
+          extra_data = "#{extra_data}&collapse_key=#{notification.collapse_key}" unless notification.collapse_key.nil?
+          extra_data = "#{extra_data}&delay_while_idle=1" if notification.delay_while_idle
+          data = "#{extra_data}#{post_data}"
         end
 
         url_string = configatron.gcm_on_rails.api_url
