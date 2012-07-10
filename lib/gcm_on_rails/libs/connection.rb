73ed7b9203d7e31a8c64e7e856d6c1d9b10836ab
Dennis Ondeng <dennis@kopokopo.com>
2012-07-10 10:54:59 +0300

    Documentation 'brush up'

--- lib/gcm_on_rails/libs/connection.rb
+++ lib/gcm_on_rails/libs/connection.rb
@@ -10,9 +10,9 @@ module Gcm
           headers = {"Content-Type" => "application/json",
                      "Authorization" => "key=#{api_key}"}
 
-          data = notification.data.merge({"collapse_key" => notification.collapse_key}) unless notification.collapse_key.nil?
-          data = data.merge({"delay_while_idle" => notification.delay_while_idle}) unless notification.delay_while_idle.nil?
-          data = data.merge({"time_to_live" => notification.time_to_live}) unless notification.time_to_live.nil?
+          data = notification.data.merge({:collapse_key => notification.collapse_key}) unless notification.collapse_key.nil?
+          data = data.merge({:delay_while_idle => notification.delay_while_idle}) unless notification.delay_while_idle.nil?
+          data = data.merge({:time_to_live => notification.time_to_live}) unless notification.time_to_live.nil?
           data = data.to_json
         else   #plain text format
           headers = {"Content-Type" => "application/x-www-form-urlencoded;charset=UTF-8",
